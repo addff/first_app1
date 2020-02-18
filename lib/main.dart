909ca93d221b715516847e03f7925ef2716a4fd5
Imran Sasudin <51149025+ImranSasudin@users.noreply.github.com>
2020-02-18 14:27:57 +0800

    Lab 5

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Lab 3',
+      title: 'Flutter Lab 5',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Flutter Lab 3'),
+          title: Text('Flutter Lab 5'),
         ),
         body: Container(
           margin: const EdgeInsets.all(10),
@@ -39,8 +39,7 @@ class MyApp extends StatelessWidget {
                             subtitle: Text('Here is a second line'),
                           ),
                         ),
-                        Icon(Icons.star, color: Colors.deepOrangeAccent),
-                        Text("41")
+                        FavoriteWidget(),
                       ],
                     ),
                     Container(
@@ -96,5 +95,50 @@ class MyApp extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class FavoriteWidget extends StatefulWidget {
+  @override
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = true;
+  int _favoriteCount = 30;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: Container(
+            child: Text('$_favoriteCount'),
+          ),
+        ),
+      ],
+    );
+  }
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
   }
 }
