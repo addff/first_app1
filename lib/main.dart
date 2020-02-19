4d74fdda6ca6728f826209857d442fd62b13b22d
Imran Sasudin <51149025+ImranSasudin@users.noreply.github.com>
2020-02-19 10:56:58 +0800

    Lab 8 - CRUD SQLITE

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,19 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'second_screen.dart';
-
-class Todo {
-  final String title;
-  final String description;
-
-  Todo(this.title, this.description);
-}
+import 'note_list.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: 'Navigation Basics',
-    home: TodosScreen(),
-  ));
+  runApp(new MyApp());
 }
 
-class TodosScreen extends StatelessWidget {
-
-  final todos = List<Todo>.generate(20, (i) => Todo(
-      'Todo $i',
-      'A description of what needs to be done for Todo $i',
-    ),
-  );
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Todos'),
-        ),
-        body: ListView.builder(
-          itemCount: todos.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(todos[index].title),
-              // When a user taps the ListTile, navigate to the DetailScreen.
-              // Notice that you're not only creating a DetailScreen, you're
-              // also passing the current todo to it.
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => DetailScreen(todo: todos[index],text:todos[index].title),
-                  ),
-                );
-              },
-            );
-          },
-        )
-
+    return MaterialApp(
+      title: "NoteKeeper",
+      theme: ThemeData(
+          primarySwatch: Colors.lime
+      ),
+      home: NoteList(),
     );
   }
 }
